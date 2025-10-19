7da5492d297c014b47814bd96c6b6b04604a5c5c
rafiul <rafiulofficial396@gmail.com>
2025-10-19 19:11:08 +0600

    loaded news from api

--- src/components/Main/homelayout/Categorynews.jsx
+++ src/components/Main/homelayout/Categorynews.jsx
@@ -3,15 +3,19 @@ import { useLoaderData, useParams } from 'react-router';
 import Newsbox from './Newsbox.jsx/Newsbox';
 
 const Categorynews = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const newsData = useLoaderData();
     const filteredNewsbyId = newsData.filter(news => news.category_id === Number(id))
     console.log(filteredNewsbyId);
-    
+
     return (
         <div>
             <h2 className='text-primary font-semibold mb-4'>Dragon News Home</h2>
-            <Newsbox />
+            <div className='space-y-4'>
+                {
+                    filteredNewsbyId.map(news => <Newsbox news={news} />)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Main/homelayout/Newsbox.jsx/Newsbox.jsx
+++ src/components/Main/homelayout/Newsbox.jsx/Newsbox.jsx
@@ -3,15 +3,18 @@ import { CiBookmark, CiShare2 } from 'react-icons/ci';
 import star from '../../../../assets/star.png'
 import { FaEye } from 'react-icons/fa';
 
-const Newsbox = () => {
+const Newsbox = ({ news }) => {
+    const {author} = news;
+    console.log(news);
+    
     return (
         <div className='space-y-3 border-2 border-base-300 rounded-lg'>
             <div className='flex justify-between bg-base-300 items-center px-4 py-1'>
-                <div className='flex'>
-                    <img className='w-[50px] h-[50px] bg-red-200 rounded-full' src="" alt="" />
+                <div className='flex gap-4'>
+                    <img className='w-[50px] h-[50px] bg-red-200 rounded-full' src={author.img} alt={`Image of ${author.name}`} />
                     <div>
-                        <h2>Md. Rafiul</h2>
-                        <p>2025-06-23</p>
+                        <h2 className='text-primary font-semibold'>{author.name}</h2>
+                        <p className='text-accent'>2025-06-23</p>
                     </div>
                 </div>
                 <div className='text-2xl flex'>
@@ -21,13 +24,13 @@ const Newsbox = () => {
             </div>
             <div className='space-y-5 px-4'>
                 <h2 className='font-bold text-primary'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, velit!
+                    {news.title}
                 </h2>
-                <div className='bg-primary w-full h-[230px] rounded-lg'>
-
+                <div className='bg-primary w-full h-[250px] rounded-lg'>
+                    <img className='rounded-lg w-full h-full object-center' src={news.image_url} alt="" />
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero aut animi atque neque ut aspernatur amet adipisci veritatis, itaque facere autem quo recusandae, nostrum veniam aperiam ipsa laboriosam tempore magni distinctio maiores deleniti! Nostrum veritatis accusamus, voluptatem velit sunt ex?
+                <p className='text-accent'>
+                    {news.details}
                 </p>
                 <div className='h-[1px] w-full bg-secondary'></div>
                 <div className='flex justify-between items-center pb-5'>
@@ -39,11 +42,11 @@ const Newsbox = () => {
                             <img className='w-[20px] h-[20px]' src={star} alt="" />
                             <img className='w-[20px] h-[20px]' src={star} alt="" />
                         </div>
-                        <p>4.9</p>
+                        <p>{news.rating.number}</p>
                     </div>
                     <div className='flex items-center gap-2'>
                         <FaEye />
-                        <p>499</p>
+                        <p>{news.total_view}</p>
                     </div>
                 </div>
             </div>
