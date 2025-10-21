79fba1c85390c8311aa004d7347e901c784d77c0
rafiul <rafiulofficial396@gmail.com>
2025-10-21 20:01:18 +0600

    working on authentication

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react';
+const AuthContext = createContext();
+
+
+const AuthProvider = () => {
+    return (
+        <AuthContext>
+
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
---
+++ src/firbase/Firebase.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8TNnwWvu99-mXtXNh6us-Y3kJy--J7gM",
+  authDomain: "dragon-news-28c31.firebaseapp.com",
+  projectId: "dragon-news-28c31",
+  storageBucket: "dragon-news-28c31.firebasestorage.app",
+  messagingSenderId: "18129159301",
+  appId: "1:18129159301:web:4aa496d803c609d09995ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
